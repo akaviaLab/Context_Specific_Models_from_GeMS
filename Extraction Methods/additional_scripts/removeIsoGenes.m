5fea4eb097970b3fdbd77e3da215283f192d4c5a
Uri David Akavia <uri.david.akavia@mcgill.ca>
2017-12-03 18:52:25 -0500

    Made sure fastCore runs with new version of cobra toolbox

--- Extraction Methods/additional_scripts/removeIsoGenes.m
+++ Extraction Methods/additional_scripts/removeIsoGenes.m
@@ -4,22 +4,11 @@ function modelNew=removeIsoGenes(model)
     % Input: model with genes in the form "entrezID.x"
     % Output: model where all genes are in the form "enrezID"
     
-    genes={};
-    for i = 1:numel(model.genes)
-        str = model.genes{i};
-        genes{i} = str(1:end-2);
-    end
-    genes = unique(genes);
-        
-    model.rules=[];
-    model.rxnGeneMat=[];
+    model = rmfield(model, {'rules', 'rxnGeneMat'});
     model.genes=[];
+    model.grRules = regexprep(model.grRules, '[.]\d*', '');
     warning off all
-    for m=1:numel(model.rxns)
-        grRule = model.grRules{m,1};
-        grRule = regexprep(grRule,'[.]\d*','');
-        model = changeGeneAssociation(model,model.rxns{m,1},grRule,genes,genes);
-    end
+    model = buildRxnGeneMat(model);
     warning on all
     modelNew=model;
 end
--- Extraction Methods/fastcore/runThresholdsFastCore.m
+++ Extraction Methods/fastcore/runThresholdsFastCore.m
@@ -1,22 +1,33 @@
 function runThresholdsFastCore(figName, bb, modName)
     %initCobraToolbox
-    load(['parsedGPR_u_',modName,'.mat'])
-    load(['model_u_',modName,'.mat'])
-    load(['gene_expr_u_',modName,'.mat'])
-    load(['gene_id_u_',modName,'.mat'])
-    load(['corrRxn_u_',modName,'.mat'])
-    load(['parsedGPR_c_',modName,'.mat'])
-    load(['model_c_',modName,'.mat'])
-    load(['gene_expr_c_',modName,'.mat'])
-    load(['gene_id_c_',modName,'.mat'])
-    load(['corrRxn_c_',modName,'.mat'])
-    load(['ths_',modName,'.mat'])
-    load(['model_s_',modName,'.mat'])
-    load(['gene_expr_s_',modName,'.mat'])
-    load(['gene_id_s_',modName,'.mat'])
-    load(['corrRxn_s_',modName,'.mat'])
-    load(['parsedGPR_s_',modName,'.mat'])
-    load(['growthRate_',modName,'.mat'])
+    load(['ID_FPKM_', modName, '.mat'], 'num');
+    load(['model_u_',modName,'.mat'], 'model_u')
+    % This loads up the gene expression based on the FPKM file, so it has
+    % the option of dealing with more reactions. There are three genes
+    % (2878, 4259, 5337)
+    % and four reactions (GTHPe, LTC4Sr, PCHOLPm_hs, PCHOLPr_hs) that are 
+    % different based on the FPKM file to the existing gene_expr/gene_id structure.
+    [~, indModel, indNum] = intersect(cellfun(@str2num, model_u.genes), num(:, 1));
+    expressionData_u.gene(1:length(indModel)) = model_u.genes(indModel);
+    expressionData_u.value(1:length(indNum)) = num(indNum, 2);
+    % This sets up the original data
+%      load(['gene_expr_u_',modName,'.mat'])
+%      load(['gene_id_u_',modName,'.mat'])
+%      expressionData_u2.gene = gene_id_u;
+%      expressionData_u2.value = gene_expr_u;
+    
+    load(['model_c_',modName,'.mat'], 'model_c')
+    [~, indModel, indNum] = intersect(cellfun(@str2num, model_c.genes), num(:, 1));
+    expressionData_c.gene(1:length(indModel)) = model_c.genes(indModel);
+    expressionData_c.value(1:length(indNum)) = num(indNum, 2);
+    
+    load(['model_s_',modName,'.mat'], 'model_s')
+    [~, indModel, indNum] = intersect(cellfun(@str2num, model_s.genes), num(:, 1));
+    expressionData_c.gene(1:length(indModel)) = model_s.genes(indModel);
+    expressionData_c.value(1:length(indNum)) = num(indNum, 2);
+    
+    load(['growthRate_',modName,'.mat'], 'blb')
+    load(['gene_threshold_',modName,'.mat'], 'ths')
     
     if strcmp(figName,'U')
         %UNCONSTRAINED
@@ -32,11 +43,11 @@ function runThresholdsFastCore(figName, bb, modName)
         end
         epsil = 1e-6;
         scaling = 1e3;
-        expressionCol = mapGeneToRxn(model_u, gene_id_u, gene_expr_u, parsedGPR_u, corrRxn_u);
-        singleRun(core, gene_expr_u, gene_id_u, expressionCol, figName, model_u, epsil, scaling, ths.p10, 1, modName)
-        singleRun(core, gene_expr_u, gene_id_u, expressionCol, figName, model_u, epsil, scaling, ths.mean, 2, modName)
-        singleRun(core, gene_expr_u, gene_id_u, expressionCol, figName, model_u, epsil, scaling, ths.p25, 3, modName)
-        singleRun(core, gene_expr_u, gene_id_u, expressionCol, figName, model_u, epsil, scaling, ths.p50, 4, modName)
+        expressionCol = mapExpressionToReactions(model_u, expressionData_u);
+        singleRun(core, expressionCol, figName, model_u, epsil, scaling, ths.p10, 1, modName)
+        singleRun(core, expressionCol, figName, model_u, epsil, scaling, ths.mean, 2, modName)
+        singleRun(core, expressionCol, figName, model_u, epsil, scaling, ths.p25, 3, modName)
+        singleRun(core, expressionCol, figName, model_u, epsil, scaling, ths.p50, 4, modName)
     end
 
     if strcmp(figName,'C')
@@ -58,11 +69,11 @@ function runThresholdsFastCore(figName, bb, modName)
         end
         epsil = 1e-8;
         scaling = 1e3;
-        expressionCol = mapGeneToRxn(model_c, gene_id_c, gene_expr_c, parsedGPR_c, corrRxn_c);
-        singleRun(core, gene_expr_c, gene_id_c, expressionCol, figName, model_c, epsil, scaling, ths.p10, 1, modName)
-        singleRun(core, gene_expr_c, gene_id_c, expressionCol, figName, model_c, epsil, scaling, ths.mean, 2, modName)
-        singleRun(core, gene_expr_c, gene_id_c, expressionCol, figName, model_c, epsil, scaling, ths.p25, 3, modName)
-        singleRun(core, gene_expr_c, gene_id_c, expressionCol, figName, model_c, epsil, scaling, ths.p50, 4, modName)
+        expressionCol = mapExpressionToReactions(model_c, expressionData_c);
+        singleRun(core, expressionCol, figName, model_c, epsil, scaling, ths.p10, 1, modName)
+        singleRun(core, expressionCol, figName, model_c, epsil, scaling, ths.mean, 2, modName)
+        singleRun(core, expressionCol, figName, model_c, epsil, scaling, ths.p25, 3, modName)
+        singleRun(core, expressionCol, figName, model_c, epsil, scaling, ths.p50, 4, modName)
     end
     
     if strcmp(figName,'S')
@@ -79,7 +90,7 @@ function runThresholdsFastCore(figName, bb, modName)
         end
         epsil = 1e-6;
         scaling = 1e3;
-        expressionCol = mapGeneToRxn(model_s, gene_id_s, gene_expr_s, parsedGPR_s, corrRxn_s);
+        expressionCol = mapExpressionToReactions(model_s, expressionData_s);
         singleRun(core, gene_expr_s, gene_id_s, expressionCol, figName, model_s, epsil, scaling, ths.p10, 1, modName)
         singleRun(core, gene_expr_s, gene_id_s, expressionCol, figName, model_s, epsil, scaling, ths.mean, 2, modName)
         singleRun(core, gene_expr_s, gene_id_s, expressionCol, figName, model_s, epsil, scaling, ths.p25, 3, modName)
@@ -88,11 +99,12 @@ function runThresholdsFastCore(figName, bb, modName)
 
 end
 
-function singleRun(core, gene_exp, gene_names, expressionCol, figName, model, epsil, scaling, th, id, modName)
+function singleRun(core, expressionCol, figName, model, epsil, th, id, modName)
     tName = ['FastCore_',figName, num2str(id),'_',modName];
     disp(tName)
-    cMod = call_fastcore(model, expressionCol, core, th, epsil, scaling);
+    C = find(expressionCol >= th);
+    C = union(C, find(ismember(model.rxns, core)));
+    cMod = fastcore(model, C, epsil);
     cMod.name = tName;
-    eval([tName,'= addMinGeneField(cMod, gene_exp, gene_names, th, 1);']);
     save([tName,'.mat'],tName)
 end
